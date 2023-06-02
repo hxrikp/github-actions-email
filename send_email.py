96c5461c22a8d018f1c0f0eba01549075af2b244
Harikrishna KP <harikrishna.kp@razorpay.com>
2023-06-02 17:02:07 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -2,16 +2,15 @@ import smtplib, ssl, os
 
 port = 465
 smtp_server = "smtp.gmail.com"
-# USERNAME = os.environ.get('USER_EMAIL')
-# PASSWORD = os.environ.get('USER_PASSWORD')
-USERNAME = "hahatester10@gmail.com"
-PASSWORD = "Tester#1234"
+USERNAME = os.environ.get('USER_EMAIL')
+PASSWORD = os.environ.get('USER_PASSWORD')
 message = """
 Subject: BLAH
 
 BLAH BLAH BALH
 """
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(USERNAME, PASSWORD)
-    server.sendmail(USERNAME, USERNAME, message)
+print(USERNAME,PASSWORD)
+# context = ssl.create_default_context()
+# with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+#     server.login(USERNAME, PASSWORD)
+#     server.sendmail(USERNAME, USERNAME, message)
